58ede66bba64127f93e5a0ba8a13d00664130508
fenghaifeng <fhf11991@LY.com>
2017-06-15 16:48:07 +0800

    fix some bugs at ProgressbarView API 22

--- app/src/main/java/com/aitangba/progressbar/ProgressbarView.java
+++ app/src/main/java/com/aitangba/progressbar/ProgressbarView.java
@@ -1,65 +1,88 @@
 package com.aitangba.progressbar;
 
-import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
-import android.support.v4.content.ContextCompat;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.ColorInt;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.animation.LinearInterpolator;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created by fhf11991 on 2016/9/20.
  */
 public class ProgressbarView extends View {
 
-    private static final String TAG = "ProgressbarView";
-
-    private static final int DURATION_TIME = 360; //ms
+    private static final int STATUS_INIT = 0;
+    private static final int STATUS_FINISHED = 1;
 
     private Paint mLightPaint;
     private Paint mDeepPaint;
     private Paint mShadePaint;
 
-    private int mDeepColorId = Color.parseColor("#91E13D");
-    private int mLightColorId = Color.parseColor("#B9EC83");
-    private int mBackgroundColor = Color.WHITE;
+    private int mDeepColorId = Color.parseColor("#11c876");
+    private int mLightColorId = Color.parseColor("#41d391");
+    private int mBackgroundColor = Color.parseColor("#b6c9de");
 
     private float mDistance; //px 0 <--> 2 * mLineWidth
-    private int mLineWidth = 20; //px
+    private int mLineWidth = 200; //px
     private double mAngle = 20; //
 
     private double mStartProgress = 0;
     private double mProgress = (double) 3  / 4;
 
-    private Direction mDirection = Direction.Right;
-    private ValueAnimator mValueAnimator;
+    private Direction mDirection;
 
-    private boolean mDrawEnable = true;
+    private boolean mUseFinishColor = true; //
     private int mFinishColor;
+    private CustomHandler mCustomHandler;
 
     public enum Direction {
         Left, Right
     }
 
-    public void setFinishColor(int finishColor) {
-        mFinishColor = finishColor;
+    public ProgressbarView(Context context) {
+        this(context, null);
+    }
+
+    public ProgressbarView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ProgressbarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    private void initView() {
+        mLightPaint = new Paint();
+        mLightPaint.setColor(mLightColorId);
+
+        mDeepPaint = new Paint();
+        mDeepPaint.setColor(mDeepColorId);
+
+        mShadePaint = new Paint();
+        mShadePaint.setColor(mBackgroundColor);
+
+        mCustomHandler = new CustomHandler(this);
+
+        //set default values
+        setDirection(Direction.Right);
+        setFinishColor(Color.parseColor("#ffe8a1"));
     }
 
     public void setDirection(Direction direction) {
         mDirection = direction;
     }
 
-    /**
-     * between 0 to 100
-     * @param startProgress
-     */
-    public void setStartProgress(int startProgress) {
-        mStartProgress = (double) startProgress / 100;
+    public void setFinishColor(@ColorInt int finishColor) {
+        mFinishColor = finishColor;
     }
 
     /**
@@ -72,18 +95,32 @@ public class ProgressbarView extends View {
             mProgress = mStartProgress;
         }
 
-        if(mProgress == 1) {
-            mDrawEnable = false;
-            if(mValueAnimator.isRunning()) {
-                mValueAnimator.cancel();
-            }
+        if(mProgress == STATUS_INIT) {
             invalidate();
+        } else if(mProgress == STATUS_FINISHED) {
+            if(mUseFinishColor) {
+                stopProgress();
+            } else {
+                if(!mCustomHandler.isRunning) {
+                    mCustomHandler.start();
+                }
+            }
         } else {
-            mDrawEnable = true;
-            if(!mValueAnimator.isRunning()) {
-                mValueAnimator.start();
+            if(!mCustomHandler.isRunning) {
+                mCustomHandler.start();
             }
         }
+    }
+
+    public void setUseFinishColor(boolean useFinishColor) {
+        mUseFinishColor = useFinishColor;
+    }
+
+    public void stopProgress() {
+        if(mCustomHandler.isRunning) {
+            mCustomHandler.stop();
+        }
+        invalidate();
     }
 
     public double getProgress() {
@@ -102,48 +139,17 @@ public class ProgressbarView extends View {
         mLineWidth = lineWidth;
     }
 
-    public ProgressbarView(Context context) {
-        this(context, null);
-    }
-
-    public ProgressbarView(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    public ProgressbarView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        mLightPaint = new Paint();
-        mLightPaint.setColor(mLightColorId);
-
-        mDeepPaint = new Paint();
-        mDeepPaint.setColor(mDeepColorId);
-
-        mShadePaint = new Paint();
-        mShadePaint.setColor(mBackgroundColor);
-
-        mValueAnimator = ValueAnimator.ofFloat(0, 1).setDuration(DURATION_TIME);
-        mValueAnimator.setRepeatCount(ValueAnimator.INFINITE);
-        mValueAnimator.setInterpolator(new LinearInterpolator());
-        mValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                float mResultAnimatorValue = (float) animation.getAnimatedValue();
-                mDistance = mResultAnimatorValue *  (2 * mLineWidth);
-                invalidate();
-            }
-        });
-    }
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        if(!mDrawEnable) {  //finish the progress
-            if(mFinishColor <= 0) {
-                canvas.drawColor(Color.parseColor("#ffe8a1"));
-            } else {
-                canvas.drawColor(ContextCompat.getColor(getContext(), mFinishColor));
-            }
+        if(mProgress == STATUS_INIT){
+            canvas.drawColor(mBackgroundColor);
+            return;
+        }
+
+        if(mProgress == STATUS_FINISHED && mUseFinishColor) {
+            canvas.drawColor(mFinishColor);
             return;
         }
 
@@ -189,16 +195,60 @@ public class ProgressbarView extends View {
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        if(mDrawEnable && mValueAnimator != null) {
-            mValueAnimator.start();
+        if(mUseFinishColor && mCustomHandler != null) {
+            mCustomHandler.start();
         }
     }
 
     @Override
     protected void onDetachedFromWindow() {
-        if(mValueAnimator != null && mValueAnimator.isRunning()) {
-            mValueAnimator.cancel();
+        if(mCustomHandler != null && mCustomHandler.isRunning) {
+            mCustomHandler.stop();
         }
         super.onDetachedFromWindow();
+    }
+
+    private static class CustomHandler extends Handler {
+        private final static int MSG_NEXT = 1;
+        private final static int MSG_STOP = 2;
+        private static final int DURATION_TIME = 70; // 36ms刷新一次
+
+        private float mIndex = 0f; // 当前步伐，一个周期会刷新7次
+        private WeakReference<ProgressbarView> mWeakReference;
+        private boolean isRunning;
+
+        public CustomHandler(ProgressbarView progressbarView) {
+            mWeakReference = new WeakReference<>(progressbarView);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(mWeakReference == null|| mWeakReference.get() == null) {
+                return;
+            }
+            ProgressbarView progressbarView = mWeakReference.get();
+
+            if(msg.what == MSG_NEXT) {
+                mIndex = (mIndex + 1) % 7;
+                progressbarView.mDistance = mIndex / 7 * (2 * progressbarView.mLineWidth);
+                progressbarView.invalidate();
+
+                sendEmptyMessageDelayed(MSG_NEXT, DURATION_TIME);
+            } else if(msg.what == MSG_NEXT) {
+                removeMessages(MSG_NEXT);
+                removeMessages(MSG_STOP);
+            }
+        }
+
+        private void start() {
+            isRunning = true;
+            sendEmptyMessageDelayed(MSG_NEXT, DURATION_TIME);
+        }
+
+        private void stop() {
+            isRunning = false;
+            sendEmptyMessage(MSG_STOP);
+        }
     }
 }
